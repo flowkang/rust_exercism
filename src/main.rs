b7dc6270499f9f49707dda263c0913f5e6479d30
flow <flow@heybit.io>
2023-01-17 09:52:44 +0900

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+pub fn hello() -> &'static str {
+    "Hello, World!"
+}
+
+pub fn main_for_hello_world() {
+    println!("{}", hello());
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    main_for_hello_world();
 }
