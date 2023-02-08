c57c34ef560c397d04630487655220dfdc8e0e61
flow <flow@heybit.io>
2023-02-09 00:43:37 +0900

    anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,9 +1,9 @@
 use std::collections::{HashSet, BinaryHeap};
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    possible_anagrams.into_iter().filter(|e| {
-        word.to_lowercase().chars().collect::<BinaryHeap<char>>().into_sorted_vec() ==
-            e.to_lowercase().chars().collect::<BinaryHeap<char>>().into_sorted_vec() &&
-            word.to_lowercase() != e.to_lowercase()
+    let to_sorted_elements = |x: &str|
+        x.to_lowercase().chars().collect::<BinaryHeap<char>>().into_sorted_vec();
+    possible_anagrams.iter().filter(|e| {
+        to_sorted_elements(word) == to_sorted_elements(e) && word.to_lowercase() != e.to_lowercase()
     }).cloned().collect()
 }
